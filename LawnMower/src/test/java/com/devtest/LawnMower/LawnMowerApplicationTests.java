6d1cf53dba6447e4bc46e6090956191c54e78af4
bassamkh <bassam-el-khoury@live.com>
2021-04-14 21:24:17 +0200

    initialized project

---
+++ LawnMower/src/test/java/com/devtest/LawnMower/LawnMowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtest.LawnMower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LawnMowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
